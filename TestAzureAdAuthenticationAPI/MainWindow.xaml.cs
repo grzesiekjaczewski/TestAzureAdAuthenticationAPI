e3d42787173f09b1a257c12d849b7224ebd7f284
gjaczewski <gjaczewski@actify.com>
2023-11-30 18:26:27 +0000

    Initial commit

---
+++ TestAzureAdAuthenticationAPI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Identity.Client;
+using System.Windows;
+using System.Configuration;
+
+namespace TestAzureAdAuthenticationAPI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var wih = new System.Windows.Interop.WindowInteropHelper(this);
+            IntPtr hWnd = wih.Handle;
+
+            AuthenticationResult result = Task.Run(() => WamTokenManager.GetToken(ConfigurationManager.AppSettings["ClientId"], hWnd))
+                .GetAwaiter()
+                .GetResult();
+
+            TokenBox.Text = result.AccessToken;
+            UserName.Text = result.Account.Username;
+         }
+    }
+}
---
+++ TestAzureAdAuthenticationAPI/WamTokenManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Identity.Client;
+
+namespace TestAzureAdAuthenticationAPI
+{
+    internal class WamTokenManager
+    {
+        public static async Task<AuthenticationResult> GetToken(string clientId, IntPtr hWnd)
+        {
+            var pca = PublicClientApplicationBuilder.Create(clientId)
+                .WithBroker(new BrokerOptions(BrokerOptions.OperatingSystems.Windows))
+                .Build();
+
+            IAccount accountToLogin = (await pca.GetAccountsAsync()).FirstOrDefault();
+            if (accountToLogin == null)
+            {
+                accountToLogin = PublicClientApplication.OperatingSystemAccount;
+            }
+
+            try
+            {
+                var authResult = await pca.AcquireTokenSilent(new[] { "user.read" }, accountToLogin)
+                                            .ExecuteAsync();
+                return authResult;
+            }
+            catch (MsalUiRequiredException)
+            {
+                var authResult = await pca.AcquireTokenInteractive(new[] { "user.read" })
+                                            .WithAccount(accountToLogin)
+                                            .WithParentActivityOrWindow(hWnd)
+                                            .ExecuteAsync();
+                return authResult;
+            }
+        }
+    }
+}
